0d9c254710fee77c81ab4ab856ebcc1b24c2615d
ronin-gh <90650946+ronin-gh@users.noreply.github.com>
2022-01-26 15:07:34 -0700

    addedjs
    added js

--- hex.js
+++ hex.js
@@ -1,5 +1,13 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
-function ColorChange() {
-    document.getElementById('btn')
+btn.addEventListener('click', function () {
+    const randomNumber = getRandomNumber(); 
+    console.log(randomNumber);
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+    
 }
