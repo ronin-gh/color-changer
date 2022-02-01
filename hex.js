8e2e12cd1c7567cc1d74a94beb68b16d030a48e3
ronin-gh <90650946+ronin-gh@users.noreply.github.com>
2022-02-01 14:38:20 -0700

    completed hex
    completed hex

--- hex.js
+++ hex.js
@@ -3,11 +3,16 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-    const randomNumber = getRandomNumber(); 
-    console.log(randomNumber);
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    //console.log(hexColor);
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+    
 });
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
-    
+function getRandomNumber () {
+    return Math.floor(Math.random() * hex.length);
 }
